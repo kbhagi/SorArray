662a8267cd5d386ac9464d74c0ae244736601202
bhargavak <19756773+kbhagi@users.noreply.github.com>
2020-03-15 12:52:35 +0530

    Add files via upload

---
+++ SortArrayAscendingOrder/SortArrayAscendingOrder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortArrayAscendingOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter number: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("\n Unsorted array: ");
+            foreach (int i in arr)
+            {
+                Console.Write("\t{0}", i);
+            }
+            Array.Sort(arr);
+            Console.Write("\nSorted array: ");
+            foreach (int i in arr)
+            {
+                Console.Write("\t{0}", i);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
